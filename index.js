8110eb86617753a65d61eed8ba29e991e8f808d3
Otabek Toshtemirov <47772571+OtabekToshtemirov@users.noreply.github.com>
2024-12-15 19:34:46 +0500

    ghnjm

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { Client } = require('pg');
 const cron = require('node-cron');
 
 const app = express();
-const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
+const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { webhook: true });
 
 // PostgreSQL connection setup
 const client = new Client({
